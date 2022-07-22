14a9fd8b5c22a25bbf15e93b071c6390b6add5e6
ducmanhlai <ducmanhlai22@gmail.com>
2022-07-22 20:55:22 +0700

    tạo login gửi về token

--- src/server.js
+++ src/server.js
@@ -1,25 +1,31 @@
-import express from "express";
-import morgan from "morgan";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import initWebRoutes from "./routes";
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import initWebRoutes from './routes';
 dotenv.config();
 
 const app = express();
 
 // app.use(morgan("combined"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.set("view engine", "ejs");
-app.set("views", "./src/views");
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 const port = process.env.PORT || 8017;
-// app.get("/", (req, res) => {
-//   res.render("pages/home");
-// });
+
+app.get('/', (req, res) => {
+   res.render('pages/home');
+});
+
 initWebRoutes(app);
-app.use((req, res) => {
-  res.render("pages/404");
-})
+
+// app.use((req, res) => {
+//   res.render("pages/404");
+// });
+
 app.listen(port, () => {
-  console.log(`Starting server on  http://127.0.0.1:${port}`);
+   console.log(`Starting server on port ${port}`);
 });

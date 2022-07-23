e0748efe6586770c8639a4ff82eca8a6857963c0
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-07-23 09:07:54 +0700

    refactor config file

--- src/routes/Authen.route.js
+++ src/routes/Authen.route.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const AuthenController = require('../controllers/AuthenController');
-const router = express.Router();
-router.post('/login',AuthenController.Login);
-module.exports = router;
+// const express = require('express');
+// const AuthenController = require('../controllers/AuthenController');
+// const router = express.Router();
+// router.post('/login',AuthenController.Login);
+// module.exports = router;

2f38f6f7bc58495b85f540e6321a744584709825
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-07-24 16:14:02 +0700

    change to maint strict :)

--- src/controllers/AuthenController.js
+++ src/controllers/AuthenController.js
@@ -1,24 +1,26 @@
 const express = require('express');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const AuthenService = require('../service/authen.service');
 class AuthenController {
-    CheckLogin(req,res,next){
-       next();
-    }
-    Login(req,res,next){
-        let username= req.body.username;
-        let pass =req.body.pass; 
-        AuthenService.Login(username,pass).then((data)=>{
+   CheckLogin(req, res, next) {
+      next();
+   }
+   Login(req, res, next) {
+      let username = req.body.username;
+      let pass = req.body.pass;
+      AuthenService.Login(username, pass)
+         .then((data) => {
             let options = {
-                maxAge: 1000 * 60 * 15, // would expire after 15 minutes
-            }
-            let token = jwt.sign(data,'manhnenene');
-            res.cookie('token',token,options);
-            res.json({token});
-        }).catch((err)=>{
-            res.json({data:`${err}`});
-        });
-        //next();
-    }
+               maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+            };
+            let token = jwt.sign(data, 'manhnenene');
+            res.cookie('token', token, options);
+            res.json({ token });
+         })
+         .catch((err) => {
+            res.json({ data: `${err}` });
+         });
+      //next();
+   }
 }
-module.exports = new AuthenController;
+module.exports = new AuthenController();

bd65be0d902359e85301595967bd8792b3e846b2
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-07-26 09:09:52 +0700

    refactor

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -4,14 +4,15 @@ import PasswordHandler from '../utils/passwordHandler';
 class AuthenService {
    async login(userName, password) {
       console.log({ userName, password });
-      const [rows] = await pool.execute('select userName,password,permissioncode as role_code from account,role where userName = ? and account.role_id= role.id', [userName]);
+      const [rows] = await pool.execute(
+         'select userName,password,permissioncode as role_code from account,role where userName = ? and account.role_id= role.id',
+         [userName],
+      );
       if (rows.length > 0) {
-         let account  = rows[0];
-         if (PasswordHandler.checkMatch(password,account.password))
-          return account;
-      else return false;
+         let account = rows[0];
+         return PasswordHandler.checkMatch(password, account.password) ? account : null;
       }
-      return undefined;
+      return null;
    }
 }
 export default new AuthenService();

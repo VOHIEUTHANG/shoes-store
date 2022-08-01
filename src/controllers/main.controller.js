2a4e53e761d41cb9b358f4faba3f947f6125ecfd
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-08-01 16:28:18 +0700

    refactor:modify database and create wishlist page

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,23 +1,41 @@
-import { info } from 'node-sass';
-import brandService from '../service/brand.service';
-import categoryService from '../service/category.service';
-
 const mainController = () => ({
    getHomePage: async (req, res) => {
       const user = req.user;
       const payloadInfo = req.payload;
-      try {
-         const payload = { user: {}, isLoggedIn: false, ...payloadInfo };
-         if (user) {
-            payload.user = user;
-            payload.isLoggedIn = true;
-         }
-         res.render('pages/home', payload);
-      } catch (error) {
-         res.status(400).json({
-            message: 'get brands of categorys list occured error !',
-         });
+      const payload = { user: {}, isLoggedIn: false, ...payloadInfo };
+      if (user) {
+         payload.user = user;
+         payload.isLoggedIn = true;
       }
+      res.render('pages/home', payload);
+   },
+   getLoginPage: (req, res) => {
+      const user = req.user;
+      const payloadInfo = req.payload;
+      if (user) {
+         res.redirect('/');
+      } else {
+         res.render('pages/login', { user: {}, isLoggedIn: false, ...payloadInfo });
+      }
+   },
+   getRegisterPage: (req, res) => {
+      const user = req.user;
+      const payloadInfo = req.payload;
+      if (user) {
+         res.redirect('/');
+      } else {
+         res.render('pages/register', { user: {}, isLoggedIn: false, ...payloadInfo });
+      }
+   },
+   getWishListPage: (req, res) => {
+      const user = req.user;
+      const payloadInfo = req.payload;
+      const payload = { user: {}, isLoggedIn: false, ...payloadInfo };
+      if (user) {
+         payload.user = user;
+         payload.isLoggedIn = true;
+      }
+      res.render('pages/wishlist', payload);
    },
    get404Page: (req, res) => {
       res.render('pages/404');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
-import mainRouter from './main.route';
 import mainController from '../controllers/main.controller';
+import mainRouter from './main.route';
 import userRoute from './user.route';
 import accountRoute from './account.route';
+import wishListRoute from './wishlist.route';
 
 export default function initWebRoutes(app) {
    app.use('/api/user/', userRoute);
    app.use('/api/account/', accountRoute);
+   app.use('/api/wishlist/', wishListRoute);
    app.use('/', mainRouter);
    app.use(mainController.get404Page);
 }

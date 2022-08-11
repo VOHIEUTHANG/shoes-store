d95c20928547a46ee86515590002c1480134b0e5
Vo Hieu Thang <77570864+VOHIEUTHANG@users.noreply.github.com>
2022-08-11 11:11:40 +0700

    feat: product details page

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const productController = require('../controllers/product.controller');
 import { createResponse } from '../helpers/responseCreator';
-import productService from '../service/product.service';
 const router = express.Router();
 router.post('/create', productController.create);
 router.get('/delete');
 router.get('/update');
-router.get('/', async (req, res) => {
-   const pageNumber = req.query.page;
-   const limit = req.query.limit || 5;
-
-   try {
-      const productResult = await productService.getActiveProduct({
-         offset: (Number(pageNumber) - 1) * Number(limit),
-         limit: Number(limit),
-      });
-      res.json(productResult);
-   } catch (error) {
-      console.log(error);
-      res.json(createResponse('error', 'failed to get active product !'));
-   }
-});
+router.get('/', productController.getActiveProducts);
 
 module.exports = router;
